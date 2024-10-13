d50cb4d01b3e7dba4a25f9d4a2a0de90d27e3665
OB <lanreboris11@yahoo.com>
2024-10-13 23:17:06 +0100

    update module and outputs

--- main.tf
+++ main.tf
@@ -4,3 +4,15 @@ module "linux_public_vm" {
   prefix          = "linux-upskill"
   subscription_id = var.subscription_id
 }
+
+############ OUTPUTS #########
+
+# Output the VM's public IP and SSH private key
+output "public_ip" {
+  value = module.linux_public_vm.public_ip
+}
+
+output "admin_username" {
+  description = "Admin user name for the VM"
+  value       = module.linux_public_vm.admin_username
+}
